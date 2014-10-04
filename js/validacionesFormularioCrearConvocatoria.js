8262ee78e594de1d03ac47c3b575f69fd124befd
Johansebastian1 <johansebastiangh@gmail.com>
2014-10-04 03:35:27 -0500

    codigoConvocatoria
    validacion codigo convocatoria

--- js/validacionesFormularioCrearConvocatoria.js
+++ js/validacionesFormularioCrearConvocatoria.js
@@ -1,16 +1,33 @@
 $(document).ready(function(){
-	$('#submit-btn-1').click(validateLogin);
+	$('#submit-btn-1').click(validateConvocatoria);
+});
+$(document).ready(function(){
+	$('#submit-btn-1').click(validateCodigoConvocatoria);
 });
 
-function validateLogin(){
-	var u=$('#convocatoria');
-	if(u.length===1 && u.val()===''){
+function validateConvocatoria(){
+	var a=$('#convocatoria');
+	if(a.length===1 && a.val()===''){
 		var e1=$('#convocatoria-error');
-		e1.html('Digite los datos requeridos');
+		e1.html('Escriba el nombre de la convocatoria');
 		e1.slideDown();
 		$('#convocatoria').addClass('errorbox');
 	}else{
 		$('#convocatoria-error').slideUp().html('');
 		$('#convocatoria').removeClass('errorbox');
+	}
+}
+
+function validateCodigoConvocatoria(){
+	var b=$('#codigoConvocatoria');
+	if(b.length===1 && b.val()===''){
+		var e2=$('#codigoConvocatoria-error');
+		e2.html('Escriba el codigo de la convocatoria');
+		e2.slideDown();
+		$('#codigoConvocatoria').addClass('errorbox');
+	}
+	else{
+		$('#codigoConvocatoria-error').slideUp.html('');
+		$('#codigoConvocatoria').removeClass('errorbox');
 	}
 }
